e50019b98767d36c47448edd95d2a2d214d0abb3
Arabic-Developer <57769362+Arabic-Developer@users.noreply.github.com>
2020-04-10 03:15:20 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE/html>
+<html>
+<head>
+    <title> ARABIC DEVELOPER VIP </title>
+    <link rel="stylesheet" style="text/css" href="style.css">
+</head>
+<body>
+<center>
+
+
+
+
+<!-- Start app -->
+<center>
+<img class="aa" src="D426CF8C-1F88-4531-A7A0-4236833D87B6.jpeg">
+</center>
+<br>
+<br>
+<hr clase="ee">
+<hr clase="ee">
+<center>
+<table style='filter:shadow'><font color="#FFFF00" size="15">🔱      مــوقــع   خــاص  باشتراكات المود منيو   🔱</font></table>
+</center>
+<hr clase="ee">
+<hr clase="ee">
+<br>
+<br>
+<br>
+
+
+<br>
+<br>
+<hr clase="ee">
+<br>
+<hr clase="ee">
+<br>
+<br>
+<br>
+<p><span style="background-color: #FFFFFF"><font color="#CCEEFF" size="15"><b><marquee direction="right">الــــمـــمـــيـــز ا ت </marquee></b></font></span></<p>
+<br>
+<br>
+<br>
+<br>
+<h1> نقطة تصويب صغيرة </h1>
+<br>
+<h1>ثبات سلاح</h1>
+<br>
+<h1>ثبات سكوب</h1>
+<br>
+<h1>عدم تناثر الرصاص</h1>
+<br>
+<h1>عدم اهتزاز الكاميرا</h1>
+<br>
+<h1> اخفاء كلي للعشب </h1>
+<br>
+<h1>اخفاء الماء</h1>
+<br>
+<h1>سلايدر</h1>
+<br>
+<h1>ايم اسيست</h1>
+<br>
+<h1>ايم بوت</h1>
+<br>
+<h1>ايم بوت تلقائي</h1>
+﻿﻿﻿﻿﻿<br>
+<h1>ايم بوت قوي</h1>
+<br>
+<h1>برو سكوب </h1>
+<br>
+<h1> تكبير الاسكوب</h1>
+<br>
+<h1>قفزة عالية</h1>
+<br>
+<h1> سرعة سيارة</h1>
+﻿﻿﻿﻿﻿<br>
+<h1> نزول سريع </h1>
+<br>
+<hr classe="ee">
+<br>
+<br>
+<p><span style="background-color: #FFFFFF"><font color="#CCEEFF" size="15"><b><marquee direction="right"> ADVANTAGES </marquee></b></font></span></<p>
+<br>
+<br>
+<br>
+<br>
+<h1> SMALL CROSS </h1>
+<br>
+<h1> NO RECOIL </h1>
+<br>
+<h1>STABLE GUN </h1>
+<br>
+<h1>NO SPREAD </h1>
+<br>
+<h1>NO CAMERA RECOIL </h1>
+<br>
+<h1>HIDE GRASS </h1>
+<br>
+<h1> HIDE WATER</h1>
+<br>
+<h1>SLIDER</h1>
+<br>
+<h1>AIM ASSIST </h1>
+<br>
+<h1>AIMBOT </h1>
+<br>
+<h1>AUTO AIMBOT</h1>
+﻿﻿﻿﻿﻿<br>
+<h1>STRONG AIMBOT</h1>
+<br>
+<h1>PRO SCOPE </h1>
+<br>
+<h1>SCOPE X5 </h1>
+<br>
+<h1>HIGH JUMP </h1>
+<br>
+<h1>SPEED  CAR </h1>
+﻿﻿﻿﻿﻿<br>
+<h1>FAST LANDING </h1>
+<br>
+<hr classe="ee">
+<br>
+<br>
+
+
+     
+
+
+
+
+
+
+
+<br>
+<hr class="ee">
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<hr clase="ee">
+<br>
+<br>
+<center>
+<h2>المطورين</h2>
+<br>
+<p><span style="background-color: #FFFFFF"><font color="#3457E4" size="7"><b><marquee direction="right">AHMED ALI</marquee></b></font></span></p>
+<br>
+<br>
+<center>
+<h2>&copy Create By Ahmed Ali</h2>
+</center>
+<br>
+
+</centre>
+</body>
+</html>
